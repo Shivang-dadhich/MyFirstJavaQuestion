8cf3148968824f18ecd4e99966a164d8186309d0
shivang-dadhich <shivangdadhich344@gamil.com>
2024-12-08 23:37:48 +0530

    this is my first question of java

---
+++ que1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class que1{
+    public static void main(String []args){
+        System.out.println("enter the radius");
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        System.out.println("Area of circle of radius"+ r + "is"+3.14*(r*r));
+
+    }
+}
